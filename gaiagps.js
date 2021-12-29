845d5b4fe3ada2ddca10e5ded49b24c818066152
Tom Woodard <woodard.thomas@gmail.com>
2021-12-29 05:53:27 -0800

    minor update

---
+++ gaiagps.js
@@ -0,0 +1,21 @@
+/**
+ * @license gaiagps.js v1.0.0 12/27/2021
+ *
+ * Copyright (c) 2021, tom Woodard 
+ * Licensed under the MIT license.
+ **/
+ 
+ const https = require('https');
+
+class gaiagps {
+  constructor(profileId) {
+    this.profileId = profileId;
+  }
+  getTracks() {
+    
+    
+    return this.tracks
+  }
+}
+
+module.exports = gaiagps;
